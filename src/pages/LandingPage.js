421f0eb9258a9bf4e87a8099c16c686e718da4d3
hungnln <nguyenhung09082000@yahoo.com>
2022-07-21 00:07:02 +0700

    first upload code demo ISC

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,50 @@
+// material
+import { Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
+// components
+import Page from '../components/Page';
+import {
+  LandingHero,
+  LandingMinimal,
+  LandingDarkMode,
+  LandingThemeColor,
+  LandingPricingPlans,
+  LandingAdvertisement,
+  LandingCleanInterfaces,
+  LandingHugePackElements
+} from '../components/_external-pages/landing';
+import EcommerceShop from './dashboard/EcommerceShop';
+
+// ----------------------------------------------------------------------
+
+const RootStyle = styled(Page)({
+  height: '100%'
+});
+
+const ContentStyle = styled('div')(({ theme }) => ({
+  overflow: 'hidden',
+  position: 'relative',
+  backgroundColor: theme.palette.background.default
+}));
+
+// ----------------------------------------------------------------------
+
+export default function LandingPage() {
+  return (
+    <RootStyle title="The starting point for your next project | Minimal-UI" id="move_top">
+      {/* <LandingHero /> */}
+      <ContentStyle>
+        <Box sx={{ height: '100px' }} />
+        <EcommerceShop />
+
+        {/* <LandingMinimal />
+        <LandingHugePackElements />
+        <LandingDarkMode />
+        <LandingThemeColor />
+        <LandingCleanInterfaces />
+        <LandingPricingPlans />
+        <LandingAdvertisement /> */}
+      </ContentStyle>
+    </RootStyle>
+  );
+}
